5c8c9be3ccbf2e4ccfeec4e04ed4b56a3f380eac
Purva Sharma <71826879+purvasharmadev@users.noreply.github.com>
2022-05-07 16:49:17 +0530

    feat: adding auth into pages

--- src/Pages/Nav/nav.js
+++ src/Pages/Nav/nav.js
@@ -1,11 +1,14 @@
 import React from "react";
-import './nav.css'
+import "./nav.css";
 import { Link } from "react-router-dom";
 import logo from "../../Assets/images/landing.svg";
+// Context
+import { useAuth } from "../../Auth/auth-context";
 
 function Nav() {
+  const { isLoggedIn, signInWithGoogle, signOut } = useAuth();
 
-    // function to toggle on small screen
+  // function to toggle on small screen
   function ClickHandler() {
     const navList = document.getElementsByClassName("navbar-list")[0];
     return navList.classList.toggle("toggle-active");
@@ -34,10 +37,16 @@ function Nav() {
 
       <ul className="navbar-list">
         {/* Explore */}
-      <li className="nav-item">
-          <Link to="/login" className="nav-link link">
-          Login
-          </Link>
+        <li className="nav-item">
+          {isLoggedIn ? (
+            <span onClick={signOut} className="nav-link link cursor-pointer">
+              Log Out
+            </span>
+          ) : (
+            <span onClick={signInWithGoogle} className="nav-link link cursor-pointer">
+              Log In
+            </span>
+          )}
         </li>
       </ul>
     </nav>
