4620db662d98c34710804b0777fe2550311fd44b
Purva Sharma <71826879+purvasharmadev@users.noreply.github.com>
2022-05-07 16:48:16 +0530

    feat: hook for accessing local Storage

---
+++ src/Hooks/useLocalStorage.js
@@ -0,0 +1,8 @@
+export const getDataFromLocal = (key, defaultValue) => {
+    const keyCheck = localStorage.getItem(key);
+    if (keyCheck) {
+      return JSON.parse(keyCheck);
+    } else {
+      return defaultValue;
+    }
+  };
